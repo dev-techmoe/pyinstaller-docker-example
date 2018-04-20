c493dd90a5c68192de5ed6bb35bd919c4fa323f3
techmoe <i@lolicookie.com>
2018-04-20 22:02:55 +0800

    fix

--- devscript/build.sh
+++ devscript/build.sh
@@ -2,6 +2,10 @@
 
 chmod +x ./devscript/*.sh
 
+echo "TRAVIS_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER}"
+echo "BUILD_APP_ENTRY=${BUILD_APP_ENTRY}"
+echo "BUILD_OUTPUT_FILE_NAME=${BUILD_OUTPUT_FILE_NAME}"
+
 # pull the docker images and create the container for build
 docker pull cdrx/pyinstaller-windows:python3
 docker run -v "$(pwd):/src/" \
